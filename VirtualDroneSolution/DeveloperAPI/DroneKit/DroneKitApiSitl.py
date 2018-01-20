12074677cb9e2c81d8633c1a1018699baf982cd7
Abraham Arce <xe1gyq@gmail.com>
2018-01-20 16:27:27 -0600

    Update DroneKitApiSitl.py

--- VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitApiSitl.py
+++ VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitApiSitl.py
@@ -4,8 +4,7 @@ import dronekit_sitl
 
 print "Start DroneKit Simulator (SITL)"
 sitl = dronekit_sitl.start_default()
-#connection_string = sitl.connection_string()
-connection_string = '127.0.0.1:14560'
+connection_string = sitl.connection_string()
 
 # Import DroneKit-Python
 from dronekit import connect, VehicleMode
