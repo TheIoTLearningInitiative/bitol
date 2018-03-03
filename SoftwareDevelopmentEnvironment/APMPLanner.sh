a43edac9fd245faa2d7d27b02de8d93f43990650
Abraham Arce <xe1gyq@gmail.com>
2018-03-03 12:02:34 -0600

    Update APMPLanner.sh

--- SoftwareDevelopmentEnvironment/APMPLanner.sh
+++ SoftwareDevelopmentEnvironment/APMPLanner.sh
@@ -22,9 +22,9 @@ cd $HOME
 
 cd $DIRECTORY_DOWNLOADS
 wget http://firmware.ardupilot.org/Tools/APMPlanner/apm_planner_2.0.24_xenial64.deb
-sudo dpkg -i apm_planner*.deb
-sudo apt-get -f install
-sudo dpkg -i apm_planner*.deb
+sudo dpkg -y -i apm_planner*.deb
+sudo apt -y -f install
+sudo dpkg -y -i apm_planner*.deb
 
 cd $HOME
 
