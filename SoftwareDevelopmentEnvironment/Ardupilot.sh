97b7eba4360bade6334fe71bbd5b7a92f1e6c5a6
Abraham Arce <xe1gyq@gmail.com>
2018-03-03 09:38:34 -0600

    Create Ardupilot.sh

---
+++ SoftwareDevelopmentEnvironment/Ardupilot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+export DIRECTORY_SOURCE=$HOME/src/
+export DIRECTORY_ARDUPILOT=$DIRECTORY_SOURCE/ardupilot/
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+cd $HOME
+
+sudo apt-get install -y python-matplotlib python-serial python-wxgtk3.0 python-wxtools python-lxml
+sudo apt-get install -y python-scipy python-opencv ccache gawk git python-pip python-pexpect
+
+cat << EOM >> $HOME/.bashrc
+
+export PATH=$PATH:$HOME/src/ardupilot/Tools/autotest
+export PATH=/usr/lib/ccache:$PATH
+
+EOM
+
+source $HOME/.bashrc
+
+cd $DIRECTORY_SOURCE
+git clone git://github.com/ArduPilot/ardupilot.git
+cd $DIRECTORY_ARDUPILOT
+git submodule update --init --recursive
+
+cd $HOME
+
+# End of File
