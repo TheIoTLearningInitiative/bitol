02b1b9dd8af6ad54eaf479f883f6a1d2814b8014
Abraham Arce <xe1gyq@gmail.com>
2018-03-03 09:12:24 -0600

    Update OpenCV.sh

--- SoftwareDevelopmentEnvironment/OpenCV.sh
+++ SoftwareDevelopmentEnvironment/OpenCV.sh
@@ -67,7 +67,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
       -D INSTALL_PYTHON_EXAMPLES=ON \
       -D INSTALL_C_EXAMPLES=OFF \
-      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
+      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
       -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python \
       -D BUILD_EXAMPLES=ON ..
 
