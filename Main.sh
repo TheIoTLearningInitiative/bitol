866e02324737bf07a21576df4fae5338c1565d81
Abraham Arce <xe1gyq@gmail.com>
2018-03-17 08:22:28 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -8,7 +8,7 @@ set -x
 
 export MAIN_PID=$$
 export MAIN_PID_PATH="/tmp/main"
-export MAIN_DIRECTORY=dirname "$(readlink -f "$0")"
+export MAIN_DIRECTORY=`dirname "$(readlink -f "$0")"`
 export MAIN_ARGUMENT_ACTION=$1
 
 export JMAVSIM=$MAIN_DIRECTORY/VirtualDroneSolution/FlightCode/SoftwareInTheLoop/JMAVSim/
