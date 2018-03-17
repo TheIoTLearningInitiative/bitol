cd900fd9e1e6d09236131aef995d04881e64d45a
Abraham Arce <xe1gyq@gmail.com>
2018-03-17 15:40:13 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -13,6 +13,7 @@ export MAIN_ARGUMENT_ACTION=$1
 
 export JMAVSIM=$MAIN_DIRECTORY/VirtualDroneSolution/FlightCode/SoftwareInTheLoop/JMAVSim/
 export SIMVEHICLE=$MAIN_DIRECTORY/VirtualDroneSolution/FlightCode/SoftwareInTheLoop/ArdupilotSitl/
+export QGROUNDCONTROL=$MAIN_DIRECTORY/VirtualDroneSolution/GroundControlStation/QGroundControl.sh
 
 # =============================================================================
 # Functions
