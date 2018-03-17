8e367a1fa9729ae752c85b9f477b7dfdb999fe81
Abraham Arce <xe1gyq@gmail.com>
2018-03-17 16:32:10 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -14,6 +14,7 @@ export MAIN_ARGUMENT_ACTION=$1
 export JMAVSIM=$MAIN_DIRECTORY/VirtualDroneSolution/FlightCode/SoftwareInTheLoop/JMAVSim/
 export SIMVEHICLE=$MAIN_DIRECTORY/VirtualDroneSolution/FlightCode/SoftwareInTheLoop/ArdupilotSitl/
 export QGROUNDCONTROL=$MAIN_DIRECTORY/VirtualDroneSolution/GroundControlStation/
+export DRONEKIT=$MAIN_DIRECTORY/VirtualDroneSolution/DeveloperAPI/DroneApi/DroneKit.sh
 
 # =============================================================================
 # Functions
@@ -28,5 +29,6 @@ export QGROUNDCONTROL=$MAIN_DIRECTORY/VirtualDroneSolution/GroundControlStation/
 export PATH=$PATH:$JMAVSIM
 export PATH=$PATH:$SIMVEHICLE
 export PATH=$PATH:$QGROUNDCONTROL
+export PATH=$PATH:$DRONEKIT
 
 # End Of File
