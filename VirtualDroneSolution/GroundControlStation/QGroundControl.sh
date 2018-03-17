c8e2e94bbb18bd1e3ef735ce20a3c0abd7d4e319
Abraham Arce <xe1gyq@gmail.com>
2018-03-17 15:38:54 -0600

    Create QGroundControl.sh

---
+++ VirtualDroneSolution/GroundControlStation/QGroundControl.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+export QGROUNDCONTROL_PID=$$
+export QGROUNDCONTROL_PID_PATH=$HOME/qgroundcontrol"
+export QGROUNDCONTROL_ARGUMENT_ACTION=$1
+export QGROUNDCONTROL_FILE=$HOME/QGroundControl.AppImage"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+cd $HOME
+
+if [ "$QGROUNDCONTROL_ARGUMENT_ACTION" = "start" ]; then
+    echo $QGROUNDCONTROL_PID > $QGROUNDCONTROL_PID_PATH
+    $QGROUNDCONTROL_FILE
+elif [ "$QGROUNDCONTROL_ARGUMENT_ACTION" = "stop" ]; then
+    QGROUNDCONTROL_PID=`cat $QGROUNDCONTROL_PID_PATH`
+    kill -- -$(ps -o pgid=$QGROUNDCONTROL_PID | grep -o [0-9]*)
+fi
+
+cd $HOME
+
+# End Of File
