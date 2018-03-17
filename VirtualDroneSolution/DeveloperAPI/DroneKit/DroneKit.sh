4517d26a66a80eba0e984af97445a838bc4d36cc
Abraham Arce <xe1gyq@gmail.com>
2018-03-17 17:12:31 -0600

    Update DroneKit.sh

--- VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKit.sh
+++ VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKit.sh
@@ -10,6 +10,7 @@ export DRONEKIT_PID=$$
 export DRONEKIT_PID_PATH=$HOME/dronekit
 export DRONEKIT_ARGUMENT_ACTION=$1
 export DRONEKIT_DIRECTORY=$HOME/Bitol/VirtualDroneSolution/DeveloperAPI/DroneKit/
+export DRONEKIT_PYTHON=$DRONEKIT_DIRECTORY/DroneKitPython.py
 export DRONEKIT_PX4=$DRONEKIT_DIRECTORY/DroneKitPythonPx4.py
 
 # =============================================================================
@@ -26,15 +27,19 @@ cd $HOME
 
 if [ "$DRONEKIT_ARGUMENT_ACTION" = "start" ]; then
     echo $DRONEKIT_PID > $DRONEKIT_PID_PATH
-    JMAVSim.sh start &
+    #JMAVSim.sh start &
+    dronekit-sitl copter --home=20.6207591,-103.3991668,30,0
     sleep 5
-    mavproxy.py --master=udp:127.0.0.1:14550 \
-                --out=udpout:127.0.0.1:14570 \
-                --out=udpout:127.0.0.1:14580 &
+    #mavproxy.py --master=udp:127.0.0.1:14550 \
+    #            --out=udpout:127.0.0.1:14570 \
+    #            --out=udpout:127.0.0.1:14580 &
+    mavproxy.py --master=tcp:127.0.0.1:5760 \
+                --out=udpout:127.0.0.1:14550 \
+                --out=udpout:127.0.0.1:1456
     sleep 5
     QGroundControl.sh start &
     sleep 5
-    python $DRONEKIT_PX4
+    python $DRONEKIT_PYTHON
 elif [ "$DRONEKIT_ARGUMENT_ACTION" = "stop" ]; then
     QGroundControl.sh stop &
     sleep 1
