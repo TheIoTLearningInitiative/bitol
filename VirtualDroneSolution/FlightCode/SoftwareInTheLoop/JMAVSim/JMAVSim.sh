a48701f758b351163b086cc34a80c89ecec50f9e
Abraham Arce <xe1gyq@gmail.com>
2018-03-17 08:05:56 -0600

    Update JMAVSim.sh

--- VirtualDroneSolution/FlightCode/SoftwareInTheLoop/JMAVSim/JMAVSim.sh
+++ VirtualDroneSolution/FlightCode/SoftwareInTheLoop/JMAVSim/JMAVSim.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+set -x
+
 # =============================================================================
 # Variables
 # =============================================================================
+
+export JMAVSIM_PID=$$
+export JMAVSIM_PID_PATH="/tmp/jmavsim"
+export JMAVSIM_ARGUMENT_ACTION=$1
 
 export DIRECTORY_SOURCE=$HOME/src/
 export DIRECTORY_PX4=$DIRECTORY_SOURCE/Firmware
@@ -11,7 +17,15 @@ export DIRECTORY_PX4=$DIRECTORY_SOURCE/Firmware
 # Functions
 # =============================================================================
 
-# None
+list_descendants ()
+{
+  local children=$(ps -o pid= --ppid "$1")
+  for pid in $children
+  do
+    list_descendants "$pid"
+  done
+  echo "$children"
+}
 
 # =============================================================================
 # Main
@@ -19,8 +33,14 @@ export DIRECTORY_PX4=$DIRECTORY_SOURCE/Firmware
 
 cd $HOME
 
-cd $DIRECTORY_PX4
-make posix jmavsim
+if [ "$LOCAL_DEVICE" = "Amikoo" ]; then
+    echo $JMAVSIM_PID > $JMAVSIM_PID_PATH
+    cd $DIRECTORY_PX4
+    make posix jmavsim
+elif [ "$LOCAL_DEVICE" = "NohochTata" ]; then
+    cat $JMAVSIM_PID_PATH
+    JMAVSIM_PID=kill $(list_descendants ${LOCAL_PID})
+fi
 
 cd $HOME
 
