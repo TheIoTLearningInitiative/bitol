348d3bd4177b49d17332761184de88290b9c6ed1
Abraham Arce <xe1gyq@gmail.com>
2018-03-18 09:25:39 -0600

    Update DroneKit.sh

--- VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKit.sh
+++ VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKit.sh
@@ -29,19 +29,34 @@ if [ "$DRONEKIT_ARGUMENT_ACTION" = "start" ]; then
 
     echo $DRONEKIT_PID > $DRONEKIT_PID_PATH
     
+    # =========================================================================
+    # SITL: DroneKit-Sitl <> PX4 JMAVSim <> Ardupilot SimVehicle
+    # =========================================================================
     # screen -dm -S dronekit-sitl dronekit-sitl copter --home=20.6207591,-103.3991668,30,0 &
     # JMAVSim.sh start &
     SimVehicle.sh start &
     sleep 10
-    
+
+    # =========================================================================
+    # Communications: MavProxy
+    # =========================================================================
+
     # screen -dm -S mavproxy mavproxy.py --master=tcp:127.0.0.1:5760 --out=udpout:127.0.0.1:14550 --out=udpout:127.0.0.1:14560
     # screen -dm -S mavproxy.py --master=udp:127.0.0.1:14550 --out=udpout:127.0.0.1:14570 --out=udpout:127.0.0.1:14580
     # None
     # sleep 10
     
+    # =========================================================================
+    # Ground Control Stations: QGroundControl
+    # =========================================================================
+   
     QGroundControl.sh start &
     sleep 10
-    
+
+    # =========================================================================
+    # Developer API: DroneKit
+    # =========================================================================
+
     python $DRONEKIT_PYTHON_PX4
     
 elif [ "$DRONEKIT_ARGUMENT_ACTION" = "stop" ]; then
@@ -50,8 +65,6 @@ elif [ "$DRONEKIT_ARGUMENT_ACTION" = "stop" ]; then
     sleep 1
     SimVehicle.sh stop &
     sleep 1
-    #killall -9 dronekit-sitl
-    #sleep 1
     DRONEKIT_PID=`cat $DRONEKIT_PID_PATH`
     kill -- -$(ps -o pgid=$DRONEKIT_PID | grep -o [0-9]*)
     
