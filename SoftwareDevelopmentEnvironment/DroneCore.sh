057a7fc2e8b6ef8d0f18589fa34c58f4cb9a0446
Abraham Arce <xe1gyq@gmail.com>
2018-03-18 09:13:13 -0600

    Create DroneCore.sh

---
+++ SoftwareDevelopmentEnvironment/DroneCore.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+export DIRECTORY_SOURCE=$HOME/src/
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+cd $HOME
+
+sudo apt-get update -y
+sudo apt-get -y install cmake build-essential colordiff astyle git libcurl4-openssl-dev doxygen
+
+cd $DIRECTORY_SOURCE
+git clone https://github.com/dronecore/DroneCore.git
+cd DroneCore
+git checkout master
+git submodule update --init --recursive
+make default
+
+# Building Error!
+
+cd $HOME
+
+# End of File
