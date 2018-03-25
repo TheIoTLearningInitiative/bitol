270a2c349d6a0b53c416794be575423788d6000d
Abraham Arce <xe1gyq@gmail.com>
2018-03-25 00:12:45 -0600

    Update FaceDetector.py

--- ComputerVision/OpenCV/FaceDetection/FaceDetector.py
+++ ComputerVision/OpenCV/FaceDetection/FaceDetector.py
@@ -11,18 +11,17 @@ if __name__ == "__main__":
 
     while True:
    
-        faceCascade = cv2.CascadeClassifier(cascPath)
         ret, image = videoCapture.read()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faceCascade = cv2.CascadeClassifier(cascPath)
         faces = faceCascade.detectMultiScale(
             gray,
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30),
-            #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
         )
 
-        #print "Found {0} faces!".format(len(faces))
+        print "Found {0} faces!".format(len(faces))
 
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
