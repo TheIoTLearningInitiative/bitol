5194dd122e51b1b1dc92f3477999f8981c848fb3
Abraham Arce <xe1gyq@gmail.com>
2018-03-24 22:06:27 -0600

    Update SimVehicle.sh

--- VirtualDroneSolution/FlightCode/SoftwareInTheLoop/ArdupilotSitl/SimVehicle.sh
+++ VirtualDroneSolution/FlightCode/SoftwareInTheLoop/ArdupilotSitl/SimVehicle.sh
@@ -29,7 +29,7 @@ if [ "$SIMVEHICLE_ARGUMENT_ACTION" = "start" ]; then
     echo $SIMVEHICLE_PID > $SIMVEHICLE_PID_PATH
     cd $DIRECTORY_ARDUPILOT
     source $HOME/.bashrc
-    sim_vehicle.py -v ArduCopter -l 20.654544,-103.3930971,30,0
+    sim_vehicle.py -v ArduCopter -l $MAIN_LAT,MAIN_LON,30,0
 elif [ "$SIMVEHICLE_ARGUMENT_ACTION" = "stop" ]; then
     SIMVEHICLE_PID=`cat $SIMVEHICLE_PID_PATH`
     kill -- -$(ps -o pgid=$SIMVEHICLE_PID | grep -o [0-9]*)
