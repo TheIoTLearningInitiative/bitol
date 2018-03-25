1ca52516e981be271ffaa987023a088f958564eb
Abraham Arce <xe1gyq@gmail.com>
2018-03-25 10:49:25 -0600

    Update FaceDetector.py

--- ComputerVision/OpenCV/FaceDetection/FaceDetector.py
+++ ComputerVision/OpenCV/FaceDetection/FaceDetector.py
@@ -9,9 +9,10 @@ import sys
 
 if __name__ == "__main__":
 
+    filePath = os.path.dirname(os.path.realpath(__file__)))
+    print (print filePath)
     cascPath = "haarcascade_frontalface_alt.xml"
     devVideo = os.environ['MAIN_DEV_VIDEO']
-    print(devVideo)
     videoCapture = cv2.VideoCapture(devVideo)
 
     while True:
