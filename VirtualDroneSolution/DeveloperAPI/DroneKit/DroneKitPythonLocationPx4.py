3035ec15a2de9363a1a97208af1bb8897e97b790
Abraham Arce <xe1gyq@gmail.com>
2018-03-24 20:54:32 -0600

    Update DroneKitPythonLocationPx4.py

--- VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitPythonLocationPx4.py
+++ VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitPythonLocationPx4.py
@@ -16,24 +16,29 @@ import time, sys, argparse, math
 # Settings
 ################################################################################################
 
-connection_string       = '127.0.0.1:14551'
-MAV_MODE_AUTO   = 4
-# https://github.com/PX4/Firmware/blob/master/Tools/mavlink_px4.py
-
-# Parse connection argument
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--connect", help="connection string")
+# Set up option parsing to get connection string
+import argparse
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
-if args.connect:
-    connection_string = args.connect
+connection_string = args.connect
+connection_string = "127.0.0.1:14551"
+sitl = None
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
 
 ################################################################################################
 # Init
 ################################################################################################
 
 # Connect to the Vehicle
-print "Connecting"
+print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
 
 def PX4setMode(mavMode):
@@ -62,11 +67,46 @@ def get_location_offset_meters(original_location, dNorth, dEast, alt):
     newlon = original_location.lon + (dLon * 180/math.pi)
     return LocationGlobal(newlat, newlon,original_location.alt+alt)
 
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
 ################################################################################################
 # Listeners
 ################################################################################################
 
-home_position_set = False
+home_position_set = True
 
 #Create a message listener for home position fix
 @vehicle.on_message('HOME_POSITION')
@@ -91,7 +131,9 @@ print " GPS: %s" % vehicle.gps_0
 print " Alt: %s" % vehicle.location.global_relative_frame.alt
 
 # Change to AUTO mode
-PX4setMode(MAV_MODE_AUTO)
+# PX4setMode(MAV_MODE_AUTO)
+
+arm_and_takeoff(10)
 time.sleep(1)
 
 # Load commands
