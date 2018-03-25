fa57d6634ae76732073f0d37720e14edcb066a31
Abraham Arce <xe1gyq@gmail.com>
2018-03-24 20:39:50 -0600

    Update DroneKitPythonAltitude.py

--- VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitPythonAltitude.py
+++ VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitPythonAltitude.py
@@ -6,15 +6,27 @@ from pymavlink import mavutil
 import time, sys, argparse, math
 from dronekit import connect, Command, LocationGlobal, VehicleMode, LocationGlobalRelative
  
-import argparse  
-parser = argparse.ArgumentParser()
-parser.add_argument('--connect', default='127.0.0.1:14551')
+
+# Set up option parsing to get connection string
+import argparse
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
- 
+
+connection_string = args.connect
+connection_string = "127.0.0.1:14551"
+sitl = None
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
 # Connect to the Vehicle
-print 'Connecting to vehicle on: %s' % args.connect
-#vehicle = connect(args.connect, baud=57600, wait_ready=True, heartbeat_timeout=60)
-vehicle = connect(args.connect, wait_ready=False)
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
  
 print " Type: %s" % vehicle._vehicle_type
 print " Armed: %s" % vehicle.armed
@@ -22,56 +34,45 @@ print " System status: %s" % vehicle.system_status.state
 print " GPS: %s" % vehicle.gps_0
 print " Alt: %s" % vehicle.location.global_relative_frame.alt
  
-def PX4setMode(mavMode):
-    vehicle._master.mav.command_long_send(vehicle._master.target_system, vehicle._master.target_component,
-                                               mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
-                                               mavMode,
-                                               0, 0, 0, 0, 0, 0)
- 
-# Function to arm and then takeoff to a user specified altitude
 def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
  
-  print "Basic pre-arm checks"
-  # Don't let the user try to arm until autopilot is ready
-  #while not vehicle.is_armable:
-    #print " Waiting for vehicle to initialise..."
-    #time.sleep(1)
-        #
-  print "Arming motors"
-  # Copter should arm in GUIDED mode
-  #vehicle.mode    = VehicleMode("GUIDED")
-  #vehicle.mode    = VehicleMode("POSCTL")
-  MAV_MODE_AUTO   = 4
- 
-  PX4setMode(MAV_MODE_AUTO)
-  time.sleep(1)
-  vehicle.armed   = True
- 
-  while not vehicle.armed:
-    print " Waiting for arming..."
-    time.sleep(1)
- 
-  print "Taking off!"
-  vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
- 
-  home = vehicle.location.global_frame
-  print home
-  time.sleep(2)
- 
-  # Check that vehicle has reached takeoff altitude
-  while True:
-    try:
-      print " Altitude: ", vehicle.location.global_relative_frame.alt 
-      #Break and return from function just below target altitude.        
-      if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
-        print "Reached target altitude"
-        break
-      time.sleep(1)
-    except KeyboardInterrupt:
-      break
-      #sys.exit()
- 
- 
+cmds = vehicle.commands
+cmds.clear()
+cmds.upload()
+
 # Initialize the takeoff sequence to 20m
 arm_and_takeoff(20)
  
@@ -86,3 +87,7 @@ vehicle.mode = VehicleMode("RTL")
  
 # Close vehicle object
 vehicle.close()
+
+# Shut down simulator if it was started.
+if sitl:
+    sitl.stop()
