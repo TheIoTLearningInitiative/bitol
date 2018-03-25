4010b52b369fe7a3b6a03e67d06673c99e240ed8
Abraham Arce <xe1gyq@gmail.com>
2018-03-25 00:03:37 -0600

    Create FaceDetector.py

---
+++ ComputerVision/OpenCV/FaceDetection/FaceDetector.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import cv2
+import os
+import sys
+
+if __name__ == "__main__":
+
+    cascPath = "haarcascade_frontalface_alt.xml"
+    videoCapture = cv2.VideoCapture("/dev/video0")
+
+    while True:
+   
+        faceCascade = cv2.CascadeClassifier(cascPath)
+        image = videoCapture.read()
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
+
+        print "Found {0} faces!".format(len(faces))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow('Face Detector', image)
+     
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    sys.exit(0)
