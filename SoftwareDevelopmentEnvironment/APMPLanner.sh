de3c63b8802ec59152dfe915f20c1fca06ac5df9
Abraham Arce <xe1gyq@gmail.com>
2018-03-29 17:24:58 -0600

    Update APMPLanner.sh

--- SoftwareDevelopmentEnvironment/APMPLanner.sh
+++ SoftwareDevelopmentEnvironment/APMPLanner.sh
@@ -6,7 +6,7 @@ set -x
 # Variables
 # =============================================================================
 
-export DIRECTORY_DOWNLOADS=$HOME/Downloads/
+# None
 
 # =============================================================================
 # Functions
@@ -18,14 +18,10 @@ export DIRECTORY_DOWNLOADS=$HOME/Downloads/
 # Main
 # =============================================================================
 
-cd $HOME
-
-cd $DIRECTORY_DOWNLOADS
 wget http://firmware.ardupilot.org/Tools/APMPlanner/apm_planner_2.0.24_xenial64.deb
 sudo dpkg -i apm_planner*.deb
 sudo apt -y -f install
 sudo dpkg -i apm_planner*.deb
-
-cd $HOME
+rm apm_planner_2.0.24_xenial64.deb
 
 # End of File
