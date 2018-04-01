42cc3238a321f16fbf9dc7d7352b420c13b4865f
Abraham Arce <xe1gyq@gmail.com>
2018-04-01 18:33:47 -0500

    Update Tasks.sh

--- VirtualDroneSolution/UseCases/XunanKab/Tasks/Tasks.sh
+++ VirtualDroneSolution/UseCases/XunanKab/Tasks/Tasks.sh
@@ -11,6 +11,11 @@ while [  $COUNTER -lt 5 ]; do
         RANDOMLAT=`echo $((1 + RANDOM % 100))`
         RANDOMLON=`echo $((1 + RANDOM % 100))`
         python GoTo.py --connect=tcp:172.17.0.${COUNTER}:5763 --lat=20.65${RANDOMLAT}009 --long=-103.39${RANDOM}602 --alt=5 &
+    elif [ "$TASK" = "goto" ]; then
+        RANDOMLAT=`echo $((1 + RANDOM % 100))`
+        RANDOMLON=`echo $((1 + RANDOM % 100))`
+        python GoTo.py --connect=tcp:172.17.0.2:5763 --lat=20.65${RANDOMLAT}009 --long=-103.39${RANDOM}602 --alt=5 &
+        python Triangle.py --connect=tcp:172.17.0.3:5763 &
     fi
     let COUNTER=COUNTER+1
 done
