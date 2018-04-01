2307e93c1a77889de21f8fc7e9f3810c65a9801e
Abraham Arce <xe1gyq@gmail.com>
2018-04-01 11:40:28 -0500

    Update Telemetry.py

--- VirtualDroneSolution/UseCases/XunanKab/Missions/Telemetry.py
+++ VirtualDroneSolution/UseCases/XunanKab/Missions/Telemetry.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Thanks Tiziano Fiorenzani for teaching us!
+https://github.com/tizianofiorenzani
+
+This script will introduce multiple things:
+- Run the simulator inside dronekit
+- Read and handle telemetry from the UAV
+- Read and change parameters
+"""
+
+# =============================================================================
+# Libraries
+# =============================================================================
+
 import argparse
 import time
 
 from dronekit import connect, VehicleMode
+
+# =============================================================================
+# Init
+# =============================================================================
 
 parser = argparse.ArgumentParser(description='commands')
 parser.add_argument('--connect')
@@ -16,9 +34,18 @@ vehicleid = float(args.id)
 
 print("Connection to the vehicle on %s" % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
-
 vehicle.wait_ready('autopilot_version')
 print('Autopilot version: %s' % vehicle.version)
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
 
 print("Vehicle ID: %d" % vehicle.parameters['SYSID_THISMAV']) 
 vehicle.parameters['SYSID_THISMAV'] = vehicleid
