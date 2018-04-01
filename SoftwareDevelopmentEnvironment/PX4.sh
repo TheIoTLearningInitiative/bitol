7516c558c33bd9c920456ca7ab5d7c55030589b9
Abraham Arce <xe1gyq@gmail.com>
2018-04-01 00:05:24 -0600

    Update PX4.sh

--- SoftwareDevelopmentEnvironment/PX4.sh
+++ SoftwareDevelopmentEnvironment/PX4.sh
@@ -25,7 +25,9 @@ sudo apt-get autoremove
 sudo apt-get remove -y modemmanager
 sudo usermod -a -G dialout $USER
 
-wget -O - https://raw.githubusercontent.com/PX4/Devguide/master/build_scripts/ubuntu_sim_nuttx.sh | bash
+# wget -O - https://raw.githubusercontent.com/PX4/Devguide/master/build_scripts/ubuntu_sim_nuttx.sh | bash
+wget https://raw.githubusercontent.com/PX4/Devguide/master/build_scripts/ubuntu_sim_nuttx.sh
+bash ubuntu_sim_nuttx.sh
 
 cd $DIRECTORY_SOFTWAREDEVELOPMENTENVIRONMENT
 
