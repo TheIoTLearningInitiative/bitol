eebcf610f3d93f1cc6c2fffd73ca5f13c99784aa
Abraham Arce <xe1gyq@gmail.com>
2018-04-01 18:41:33 -0500

    Update Square.py

--- VirtualDroneSolution/UseCases/XunanKab/Tasks/Square.py
+++ VirtualDroneSolution/UseCases/XunanKab/Tasks/Square.py
@@ -7,33 +7,73 @@
 # Code partly based on DroneKit (c) Copyright 2015-2016, 3D Robotics.
 ################################################################################################
 
-# Import DroneKit-Python
+# =============================================================================
+# Variables
+# =============================================================================
+
+import argparse
+import math
+import sys
+import time
+
 from dronekit import connect, Command, LocationGlobal
 from pymavlink import mavutil
-import time, sys, argparse, math
 
-################################################################################################
-# Settings
-################################################################################################
+# =============================================================================
+# Init
+# =============================================================================
 
 MAV_MODE_AUTO   = 4
 # https://github.com/PX4/Firmware/blob/master/Tools/mavlink_px4.py
 
-# Parse connection argument
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--connect", help="connection string")
+parser = argparse.ArgumentParser(description='commands')
+parser.add_argument('--connect')
+parser.add_argument('--lat')
+parser.add_argument('--long')
+parser.add_argument('--alt')
 args = parser.parse_args()
 
-if args.connect:
-    connection_string = args.connect
+connection_string = args.connect
 
-################################################################################################
-# Init
-################################################################################################
-
-# Connect to the Vehicle
-print "Connecting"
+print("Connection to the vehicle on %s" % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+    vehicle.airspeed = 10
+
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        time.sleep(1)
 
 def PX4setMode(mavMode):
     vehicle._master.mav.command_long_send(vehicle._master.target_system, vehicle._master.target_component,
