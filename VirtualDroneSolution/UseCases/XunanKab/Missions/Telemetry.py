7f2299d0a6a9705960573bd1d52aab065138bba6
Abraham Arce <xe1gyq@gmail.com>
2018-04-01 09:30:45 -0500

    Update Telemetry.py

--- VirtualDroneSolution/UseCases/XunanKab/Missions/Telemetry.py
+++ VirtualDroneSolution/UseCases/XunanKab/Missions/Telemetry.py
@@ -18,29 +18,29 @@ print("Connection to the vehicle on %s" % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
 
 vehicle.wait_ready('autopilot_version')
-print('Autopilot version: %s'%vehicle.version)
+print('Autopilot version: %s' % vehicle.version)
 
-print('Supports set attitude from companion: %s'%vehicle.capabilities.set_attitude_target_local_ned)
-print('Position: %s'% vehicle.location.global_relative_frame)
-print('Attitude: %s'% vehicle.attitude)
-print('Velocity: %s'%vehicle.velocity) #- North, east, down
-print('Last Heartbeat: %s'%vehicle.last_heartbeat)
-print('Is the vehicle armable: %s'%vehicle.is_armable)
-print('Groundspeed: %s'% vehicle.groundspeed) #(%)
-print('Mode: %s'% vehicle.mode.name)
-print('Armed: %s'%vehicle.armed)
-print('EKF Ok: %s'%vehicle.ekf_ok)
+print("Vehicle ID: %d" % vehicle.parameters['SYSID_THISMAV']) 
+vehicle.parameters['SYSID_THISMAV'] = vehicleid
+time.sleep(1)
+print("Vehicle ID: %d" % vehicle.parameters['SYSID_THISMAV']) 
+
+print('Supports set attitude from companion: %s' % vehicle.capabilities.set_attitude_target_local_ned)
+print('Position: %s' % vehicle.location.global_relative_frame)
+print('Attitude: %s' % vehicle.attitude)
+print('Velocity: %s' % vehicle.velocity)
+print('Last Heartbeat: %s' % vehicle.last_heartbeat)
+print('Is the vehicle armable: %s' % vehicle.is_armable)
+print('Groundspeed: %s' % vehicle.groundspeed)
+print('Mode: %s' % vehicle.mode.name)
+print('Armed: %s' %vehicle.armed)
+print('EKF Ok: %s' %vehicle.ekf_ok)
 
 def attitude_callback(self, attr_name, value):
     print(vehicle.attitude)
 
 vehicle.add_attribute_listener('attitude', attitude_callback)
 time.sleep(5)
-vehicle.remove_attribute_listener('attitude', attitude_callback) #(.remove)
-
-print("Vehicle ID: %d" % vehicle.parameters['SYSID_THISMAV']) 
-vehicle.parameters['SYSID_THISMAV'] = vehicleid
-time.sleep(1)
-print("Vehicle ID: %d" % vehicle.parameters['SYSID_THISMAV']) 
+vehicle.remove_attribute_listener('attitude', attitude_callback)
 
 vehicle.close()
