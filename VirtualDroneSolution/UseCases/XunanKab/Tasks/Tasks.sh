31bd23d75fbf1a5bb368bf2b73944246278f03da
Abraham Arce <xe1gyq@gmail.com>
2018-04-01 18:36:02 -0500

    Update Tasks.sh

--- VirtualDroneSolution/UseCases/XunanKab/Tasks/Tasks.sh
+++ VirtualDroneSolution/UseCases/XunanKab/Tasks/Tasks.sh
@@ -5,15 +5,13 @@ TASK=$1
 COUNTER=2
 while [  $COUNTER -lt 5 ]; do
     echo The counter is $COUNTER
+    RANDOMLAT=`echo $((1 + RANDOM % 100))`
+    RANDOMLON=`echo $((1 + RANDOM % 100))`
     if [ "$TASK" = "telemetry" ]; then
         python Telemetry.py --connect=tcp:172.17.0.${COUNTER}:5763 &
     elif [ "$TASK" = "goto" ]; then
-        RANDOMLAT=`echo $((1 + RANDOM % 100))`
-        RANDOMLON=`echo $((1 + RANDOM % 100))`
         python GoTo.py --connect=tcp:172.17.0.${COUNTER}:5763 --lat=20.65${RANDOMLAT}009 --long=-103.39${RANDOM}602 --alt=5 &
     elif [ "$TASK" = "random" ]; then
-        RANDOMLAT=`echo $((1 + RANDOM % 100))`
-        RANDOMLON=`echo $((1 + RANDOM % 100))`
         python GoTo.py --connect=tcp:172.17.0.2:5763 --lat=20.65${RANDOMLAT}009 --long=-103.39${RANDOM}602 --alt=5 &
         python Triangle.py --connect=tcp:172.17.0.3:5763 &
     fi
