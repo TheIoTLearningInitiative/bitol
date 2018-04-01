b4dc169db14331afea610a98494ee1cd4d419bb4
Abraham Arce <xe1gyq@gmail.com>
2018-04-01 09:41:30 -0500

    Update Mission.py

--- VirtualDroneSolution/UseCases/XunanKab/Missions/Mission.py
+++ VirtualDroneSolution/UseCases/XunanKab/Missions/Mission.py
@@ -1,23 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
-Drone delivery: 
+Thanks Tiziano Fiorenzani for teaching us!
+https://github.com/tizianofiorenzani
 
-we are going to build a mission in mission planner, upload the mission to the drone.
-
+Drone Delivery: We are going to build a mission in mission planner, upload the mission to the drone.
 The script will connect with the vehicle and check if a new mission has been uploaded. 
 As soon as a valid mission is available, we takeoff in GUIDED mode and then we switch
-to AUTO.
-When the mission is completed we command to co back to home and land
+to AUTO. When the mission is completed we command to co back to home and land
 """
-
 
 import time
 from dronekit import connect, VehicleMode, LocationGlobalRelative, Command, LocationGlobal
 from pymavlink import mavutil
 
-#--------------------------------------------------
-#-------------- FUNCTIONS  
-#--------------------------------------------------
-#-- Define arm and takeoff
 def arm_and_takeoff(altitude):
 
    while not vehicle.is_armable:
@@ -28,7 +25,8 @@ def arm_and_takeoff(altitude):
    vehicle.mode = VehicleMode("GUIDED")
    vehicle.armed = True
 
-   while not vehicle.armed: time.sleep(1)
+   while not vehicle.armed: 
+      time.sleep(1)
 
    print("Taking Off")
    vehicle.simple_takeoff(altitude)
@@ -42,9 +40,6 @@ def arm_and_takeoff(altitude):
       time.sleep(1)
 
 def clear_mission(vehicle):
-    """
-    Clear the current mission.
-    """
     cmds = vehicle.commands
     vehicle.commands.clear()
     vehicle.flush()
@@ -57,25 +52,12 @@ def clear_mission(vehicle):
     cmds.wait_ready()
 
 def download_mission(vehicle):
-    """
-    Download the current mission from the vehicle.
-    """
     cmds = vehicle.commands
     cmds.download()
     cmds.wait_ready() # wait until download is complete.
     
 
 def get_current_mission(vehicle):
-    """
-    Downloads the mission and returns the wp list and number of WP 
-    
-    Input: 
-        vehicle
-        
-    Return:
-        n_wp, wpList
-    """
-
     print "Downloading mission"
     download_mission(vehicle)
     missionList = []
@@ -87,33 +69,26 @@ def get_current_mission(vehicle):
     return n_WP, missionList
     
 
-def add_last_waypoint_to_mission(                                       #--- Adds a last waypoint on the current mission file
-        vehicle,            #--- vehicle object
-        wp_Last_Latitude,   #--- [deg]  Target Latitude
-        wp_Last_Longitude,  #--- [deg]  Target Longitude
-        wp_Last_Altitude):  #--- [m]    Target Altitude
-    """
-    Upload the mission with the last WP as given and outputs the ID to be set
-    """
-    # Get the set of commands from the vehicle
+def add_last_waypoint_to_mission(
+        vehicle,
+        wp_Last_Latitude,
+        wp_Last_Longitude,
+        wp_Last_Altitude):
+
     cmds = vehicle.commands
     cmds.download()
     cmds.wait_ready()
 
-    # Save the vehicle commands to a list
     missionlist=[]
     for cmd in cmds:
         missionlist.append(cmd)
 
-    # Modify the mission as needed. For example, here we change the
     wpLastObject = Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, 
                            wp_Last_Latitude, wp_Last_Longitude, wp_Last_Altitude)
     missionlist.append(wpLastObject)
 
-    # Clear the current mission (command is sent when we call upload())
     cmds.clear()
 
-    #Write the modified mission and flush to the vehicle
     for cmd in missionlist:
         cmds.add(cmd)
     cmds.upload()
@@ -125,29 +100,18 @@ def ChangeMode(vehicle, mode):
             vehicle.mode = VehicleMode(mode)
             time.sleep(0.5)
     return True
-#--------------------------------------------------
-#-------------- INITIALIZE  
-#--------------------------------------------------      
-#-- Setup the commanded flying speed
+
 gnd_speed = 10 # [m/s]
 mode      = 'GROUND'
 
-#--------------------------------------------------
-#-------------- CONNECTION  
-#--------------------------------------------------    
-#-- Connect to the vehicle
 print('Connecting...')
 vehicle = connect('udp:127.0.0.1:14551')
 #vehicle = connect('tcp:127.0.0.1:5762', wait_ready=True)
 
 
-#--------------------------------------------------
-#-------------- MAIN FUNCTION  
-#--------------------------------------------------    
 while True:
     
     if mode == 'GROUND':
-        #--- Wait until a valid mission has been uploaded
         n_WP, missionList = get_current_mission(vehicle)
         time.sleep(2)
         if n_WP > 0:
@@ -156,26 +120,21 @@ while True:
             
     elif mode == 'TAKEOFF':
        
-        #-- Add a fake waypoint at the end of the mission
         add_last_waypoint_to_mission(vehicle, vehicle.location.global_relative_frame.lat, 
                                        vehicle.location.global_relative_frame.lon, 
                                        vehicle.location.global_relative_frame.alt)
         print("Home waypoint added to the mission")
         time.sleep(1)
-        #-- Takeoff
         arm_and_takeoff(10)
         
-        #-- Change the UAV mode to AUTO
         print("Changing to AUTO")
         ChangeMode(vehicle,"AUTO")
         
-        #-- Change mode, set the ground speed
         vehicle.groundspeed = gnd_speed
         mode = 'MISSION'
         print ("Swiitch mode to MISSION")
         
     elif mode == 'MISSION':
-        #-- Here we just monitor the mission status. Once the mission is completed we go back
         #-- vehicle.commands.cout is the total number of waypoints
         #-- vehicle.commands.next is the waypoint the vehicle is going to
         #-- once next == cout, we just go home
@@ -183,11 +142,10 @@ while True:
         print ("Current WP: %d of %d "%(vehicle.commands.next, vehicle.commands.count))
         if vehicle.commands.next == vehicle.commands.count:
             print ("Final waypoint reached: go back home")
-            #-- First we clear the flight mission
+
             clear_mission(vehicle)
             print ("Mission deleted")
             
-            #-- We go back home
             ChangeMode(vehicle,"RTL")
             mode = "BACK"
             
