9eadeaa5e7b044770e41a70c48fa264f6fc3a34b
Abraham Arce <xe1gyq@gmail.com>
2018-04-24 00:14:25 -0500

    Update Go.sh

--- VirtualDroneSolution/UseCases/XunanKab/Go.sh
+++ VirtualDroneSolution/UseCases/XunanKab/Go.sh
@@ -40,15 +40,16 @@ update-ca-certificates && \
 	make && \
 	make install 
 
+cd
 git clone git://github.com/ArduPilot/ardupilot.git
 cd ardupilot
-git submodule update --init --recursive; 
+git submodule update --init --recursive;
 
 cd
-
 git clone git://github.com/tridge/jsbsim.git
 cd jsbsim 
 ./autogen.sh --enable-libraries && make
+rm -rf .git
 
 ln -s /usr/include/locale.h /usr/include/xlocale.h
 sed -i 's/, int,/, unsigned int,/' /usr/include/assert.h
@@ -56,19 +57,21 @@ sed -i 's/, int,/, unsigned int,/' /usr/include/assert.h
 pip install pymavlink \
 	MAVProxy
 
-cd
-
 echo 'export PATH=$PATH:/jsbsim/src' >> /etc/profile && \
 	echo 'export PATH=$PATH:/ardupilot/Tools/autotest' >> /etc/profile && \
 	echo 'export PATH=/usr/lib/ccache:$PATH' >> /etc/profile && \
 	echo 'export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH' >> /etc/profile
 
+cd
 cd ardupilot/ArduPlane
 sed -i 's/feenableexcept(exceptions);/\/\/feenableexcept(exceptions);/' /ardupilot/libraries/AP_HAL_SITL/Scheduler.cpp  && \
 	sed -i 's/int old = fedisableexcept(FE_OVERFLOW);/int old = 1;/' /ardupilot/libraries/AP_Math/matrix_alg.cpp && \
 	sed -i 's/if (old >= 0 && feenableexcept(old) < 0)/if (0)/' /ardupilot/libraries/AP_Math/matrix_alg.cpp && \
 	sed -i "s/#include <sys\/types.h>/#include <sys\/types.h>\n\n#define TCGETS2 _IOR('T', 0x2A, struct termios2)\n#define TCSETS2 _IOW('T', 0x2B, struct termios2)/" /ardupilot/libraries/AP_HAL_SITL/UART_utils.cpp
 . /etc/profile && sim_vehicle.py -w
+cd
+cd ardupilot/
+rm -rf .git/objects/pack 
 
 apk del \
 	build-base \
@@ -85,5 +88,3 @@ apk del \
 	rm -rf /root/.config && \
 	rm -rf /root/.ash_history && \
 	rm -rf /root/.tilecache && \
-	rm -rf /jsbsim/.git && \
-	rm -rf /ardupilot/.git/objects/pack 
