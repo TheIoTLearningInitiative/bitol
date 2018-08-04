0c7137d64ba8cea8459c9325d3c4c63123533d95
Abraham Arce <xe1gyq@gmail.com>
2018-08-04 10:54:50 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitPythonAttributes.py
+++ VirtualDroneSolution/DeveloperAPI/DroneKit/DroneKitPythonAttributes.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python
 
-connection_string = '127.0.0.1:14551'
-
 # Import DroneKit-Python
 from dronekit import connect, VehicleMode
 
+# Set up option parsing to get connection string
+import argparse  
+parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
+parser.add_argument('--connect', 
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
 # Connect to the Vehicle.
-print("Connecting to vehicle on: %s" % (connection_string,))
+#   Set `wait_ready=True` to ensure default attributes are populated before `connect()` returns.
+print "\nConnecting to vehicle on: %s" % connection_string
 vehicle = connect(connection_string, wait_ready=True)
 
 # Get some vehicle attributes (state)
