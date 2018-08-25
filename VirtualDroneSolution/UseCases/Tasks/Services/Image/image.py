53a82ced330d5f150f3f36537e1e042d89048b5c
Abraham Arce <xe1gyq@gmail.com>
2018-08-25 18:45:00 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ VirtualDroneSolution/UseCases/Tasks/Services/Image/image.py
@@ -0,0 +1,33 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+framerate = 25.0
+
+#out = cv2.VideoWriter('appsrc ! videoconvert ! '
+#                      'x264enc noise-reduction=10000 speed-preset=ultrafast tune=zerolatency ! '
+#                      'rtph264pay config-interval=1 pt=96 !'
+#                      'tcpserversink host=127.0.0.1 port=5000 sync=false',
+#                      0, framerate, (640, 480))
+
+#out = cv2.VideoWriter('appsrc ! videoconvert ! video/x-raw,format=YUY2,width=640,height=480 ! jpegenc ! rtpjpegpay ! '
+#                      'udpsink host=127.0.0.1 port=5000',
+#                      0, framerate, (640, 480))
+
+out = cv2.VideoWriter('v4l2src ! video/x-raw,format=YUY2,width=640,height=480 ! jpegenc ! rtpjpegpay ! '
+                      'udpsink host=127.0.0.1 port=5000')
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+
+        out.write(frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
