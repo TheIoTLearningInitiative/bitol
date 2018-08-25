b60fbe25b69f109e0708a78e3bc07110b34e45e6
Abraham Arce <xe1gyq@gmail.com>
2018-08-25 17:12:06 -0500

    Update main.sh

--- VirtualDroneSolution/UseCases/Tasks/To/main.sh
+++ VirtualDroneSolution/UseCases/Tasks/To/main.sh
@@ -12,6 +12,7 @@ VEHICLEID=$3
 LATITUDE=$4
 LONGITUDE=$5
 ALTITUDE=$6
+SPEED=$7
 
 # =============================================================================
 # Functions
@@ -26,3 +27,4 @@ ALTITUDE=$6
 cd $HOME/
 python go.py --connect=tcp:${IPADDRESS}:${PORT} --id=${VEHICLEID} \
                --lat=${LATITUDE} --lon=${LONGITUDE} --alt=${ALTITUDE} 
+               --speed=${SPEED}
