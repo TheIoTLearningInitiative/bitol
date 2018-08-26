45439ba2a37a296e7e6747c539baf2c3fdefdfde
Linux User <opencv@opencv.my.domain>
2018-08-26 10:16:19 -0500

    Update
    Signed-off-by: Linux User <opencv@opencv.my.domain>

--- VirtualDroneSolution/UseCases/Maax/OpenCv.sh
+++ VirtualDroneSolution/UseCases/Maax/OpenCv.sh
@@ -1,25 +1,14 @@
 #!/bin/sh
 
 OPENCV_VERSION=3.4.2
-apk add --update --no-cache \
-    # Build dependencies
-    clang clang-dev cmake pkgconf wget openblas openblas-dev \
-    linux-headers \
-    # Image IO packages
-    libjpeg-turbo-dev \
-    libpng libpng-dev \
-    libwebp libwebp-dev \
-    tiff tiff-dev \
-    jasper-libs jasper-dev \
-    openexr openexr-dev \
-    # Video depepndencies
-    ffmpeg-dev \
-    libavc1394 libavc1394-dev \
-    gstreamer gstreamer-dev \
-    gst-plugins-base gst-plugins-base-dev \
-    libgphoto2 libgphoto2-dev
+apk add --update --no-cache build-base clang clang-dev cmake pkgconf wget
+apk add --update --no-cache linux-headers libjpeg-turbo libjpeg-turbo-dev libpng libpng-dev
+apk add --update --no-cache libwebp libwebp-dev tiff tiff-dev jasper-libs jasper-dev
+apk add --update --no-cache libavc1394 libavc1394-dev
+apk add --update --no-cache gstreamer gstreamer-dev gst-plugins-base gst-plugins-base-dev libgphoto2 libgphoto2-dev 
 
-# apk add --update --no-cache build-base libjpeg-turbo ffmpeg-libs
+#apk add --update --no-cache openblas openblas-dev
+#apk add --update --no-cache openexr openexr-dev ffmpeg-libs ffmpeg-dev
 
 apk add --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing \
         --update --no-cache libtbb libtbb-dev
@@ -28,7 +17,10 @@ apk add --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing \
 apk add --repository http://dl-cdn.alpinelinux.org/alpine/edge/main \
         --update --no-cache python3 python3-dev
 apk add --repository http://dl-cdn.alpinelinux.org/alpine/edge/community \
-        --update --no-cache py-numpy py-numpy-dev
+        --update --no-cache py-numpy py-numpy-dev openblas openblas-dev openexr openexr-dev ffmpeg-libs ffmpeg-dev
+
+#apk add --update --no-cache openblas openblas-dev                                                                                                                                       
+#apk add --update --no-cache openexr openexr-dev ffmpeg-libs ffmpeg-dev                                                                                                                  
 
 # Make Python3 as default
 ln -vfs /usr/bin/python3 /usr/local/bin/python
@@ -42,7 +34,7 @@ ln -vfs /usr/include/locale.h /usr/include/xlocale.h
 cd /tmp
 wget https://github.com/opencv/opencv/archive/$OPENCV_VERSION.tar.gz
 tar -xvzf $OPENCV_VERSION.tar.gz
-rm -vrf $OPENCV_VERSION.tar.gz
+#rm -vrf $OPENCV_VERSION.tar.gz
 
 # Configure
 mkdir -vp /tmp/opencv-$OPENCV_VERSION/build
@@ -54,10 +46,8 @@ cmake \
     -D CMAKE_C_COMPILER=/usr/bin/clang \
     -D CMAKE_CXX_COMPILER=/usr/bin/clang++ \
     -D CMAKE_INSTALL_PREFIX=/usr \
-    # No examples
     -D INSTALL_PYTHON_EXAMPLES=NO \
     -D INSTALL_C_EXAMPLES=NO \
-    # Support
     -D WITH_IPP=NO \
     -D WITH_1394=NO \
     -D WITH_LIBV4L=NO \
@@ -66,7 +56,6 @@ cmake \
     -D WITH_FFMPEG=YES \
     -D WITH_GPHOTO2=YES \
     -D WITH_GSTREAMER=YES \
-    # NO doc test and other bindings
     -D BUILD_DOCS=NO \
     -D BUILD_TESTS=NO \
     -D BUILD_PERF_TESTS=NO \
@@ -74,11 +63,12 @@ cmake \
     -D BUILD_opencv_java=NO \
     -D BUILD_opencv_python2=NO \
     -D BUILD_ANDROID_EXAMPLES=NO \
-    # Build Python3 bindings only
     -D PYTHON3_LIBRARY=`find /usr -name libpython3.so` \
     -D PYTHON_EXECUTABLE=`which python3` \
     -D PYTHON3_EXECUTABLE=`which python3` \
     -D BUILD_opencv_python3=YES ..
+
+exit 0
 
 # Build
 make -j`grep -c '^processor' /proc/cpuinfo`
