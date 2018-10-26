af06199ecfd22559904b627d8a03e16843fc78a6
Abraham Arce <abraham.arce.moreno@intel.com>
2018-10-26 10:45:21 -0500

    Update
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- VirtualDroneSolution/UseCases/XunanKab/xunankab.sh
+++ VirtualDroneSolution/UseCases/XunanKab/xunankab.sh
@@ -10,10 +10,15 @@ set -x
 
 SERVER_IP=$1
 
-# Variables :: Copter
+# Variables :: Core :: Copter
 
-COPTER_IMAGE="xe1gyq/copter"
-COPTER_IP=$2
+COPTER_DOCKER_IMAGE=${USER}/copter
+COPTER_NUMBER=1
+COPTER_NAMES[0]='quintanaroo-copter'
+COPTER_NAMES[1]='yucatan-copter'
+
+COPTER_GPS_COORDINATES_LATITUDE=20.6710109
+COPTER_GPS_COORDINATES_LONGITUDE=-103.4562345
 
 # Variables :: Image
 
@@ -33,11 +38,20 @@ if [ $? -ne 0 ]; then
     docker network create --subnet=${NETWORK_SUBNET}/16 ${NETWORK_NAME}
 fi
 
+for ((i=0; i<=$COPTER_NUMBER; i++)); do
+  COPTER_NAME=${COPTER_NAMES[$i]}
+  echo $COPTER_NAME
+  #docker run -itd --name ${COPTER_ONE} ${COPTER_DOCKER_IMAGE} 1 20.6552144 -103.3239878 5
+  #docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${COPTER_ONE}
+  #docker run -itd --name ${COPTER_TWO} ${COPTER_DOCKER_IMAGE} 1 20.6552144 -103.3239878 5
+  #docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${COPTER_TWO}
+done
+
 #docker run --net ${NETWORK_NAME} --ip ${COPTER_IP} -itd ${COPTER_IMAGE}
 
 # Main :: Image
 
 #docker run --net ${NETWORK_NAME} --ip ${SERVICE_FACEDETECT_IP} -it -p 5000:5000/udp -p 5600:5600/udp xe1gyq/facedetect
-docker run --net ${NETWORK_NAME} --ip ${SERVICE_FACEDETECT_IP} -itd xe1gyq/facedetect
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg,width=640, height=480, framerate=30/1 ! rtpjpegpay ! udpsink host=${SERVICE_FACEDETECT_IP} port=5000 &
-gst-launch-1.0 -v udpsrc port=5600 ! application/x-rtp, media=video, clock-rate=90000, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! xvimagesink sync=0 &
+#docker run --net ${NETWORK_NAME} --ip ${SERVICE_FACEDETECT_IP} -itd xe1gyq/facedetect
+#gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg,width=640, height=480, framerate=30/1 ! rtpjpegpay ! udpsink host=${SERVICE_FACEDETECT_IP} port=5000 &
+#gst-launch-1.0 -v udpsrc port=5600 ! application/x-rtp, media=video, clock-rate=90000, encoding-name=JPEG, payload=26 ! rtpjpegdepay ! jpegdec ! xvimagesink sync=0 &
