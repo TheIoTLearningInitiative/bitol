c5c0b575d1ea8b77e9d15297b3f5195216d40838
Abraham Arce <xe1gyq@gmail.com>
2018-11-02 08:47:04 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- VirtualDroneSolution/UseCases/XunanKab/Stream.sh
+++ VirtualDroneSolution/UseCases/XunanKab/Stream.sh
@@ -9,12 +9,14 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}" )" )"
 
 usage() {
-    echo "$0 [-i initialize] [-s <server>] [-n <name>]"
+    echo "$0 [-i initialize] [-s <server>] [-n <name>] [-c <camera>] [-p <port>]"
     echo ""
     echo "Options:"
     echo "  -i: Initialize? powerup :: powedown"
     echo "  -s: IP address of the server"
     echo "  -n: Name of the vehicle"
+    echo "  -c: Camera device"
+    echo "  -p: Stream port "
     echo ""
 }
 
@@ -29,6 +31,12 @@ while getopts "i:s:n:c:p:" o; do
         n)
             NAME="$OPTARG"
             ;;
+        c)
+            CAMERA_DEVICE="$OPTARG"
+            ;;
+        p)
+            STREAM_PORT="$OPTARG"
+            ;;
         *)
             usage
             exit 1
@@ -40,7 +48,7 @@ shift $((OPTIND-1))
 VEHICLE_NAME=$NAME-vehicle
 VEHICLE_ID=${VEHICLE_ID:-1}
 VEHICLE_LATITUDE=${VEHICLE_LATITUDE:-20.6679137}
-VEHICLE_LONGITUDE=${VEHICLE_LONGITUDE:--103.4630988}
+VEHICLE_LONGITUDE=${VEHICLE_LONGITUDE:-103.4630988}
 VEHICLE_ALTITUDE=${VEHICLE_ALTITUDE:-10}
 
 VEHICLE_DOCKER_IMAGE=user/core-copter
@@ -52,25 +60,25 @@ CONNECTION_PROTOCOL=tcp
 CONNECTION_PORT_GROUND_CONTROL_STATION=5762
 CONNECTION_PORT_COMMUNICATION_LIBRARY=5763
 
+FACEDETECT_DOCKER_IMAGE=user/computervision-facedetect
+FACEDETECT_NAME=$NAME-facedetect
+
 # =============================================================================
 # Main
 # =============================================================================
 
 if ([ "$INITIALIZE" == "powerup" ]); then
 
-    UUID=`docker run -itd --name ${VEHICLE_NAME} \
-                                 ${VEHICLE_DOCKER_IMAGE} \
-                                 ${VEHICLE_ID} \
-                                 ${VEHICLE_LATITUDE} ${VEHICLE_LONGITUDE} \
-                                 ${VEHICLE_ALTITUDE}`
-    IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${VEHICLE_NAME}`
-    echo "Copter Information: " $VEHICLE_NAME $IP $UUID
-    sleep 3
-    CONNECTION=${CONNECTION_PROTOCOL}:${IP}:${CONNECTION_PORT_COMMUNICATION_LIBRARY}
-    docker run $TASK_DOCKER_IMAGE_ID $CONNECTION $VEHICLE_ID
+    if [ -e $CAMERA_DEVICE ]; then
+
+        IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${FACEDETECT_NAME}`
+        echo "Service FaceDetect Information: " $FACEDETECT_NAME $IP
+        gst-launch-1.0 -v v4l2src device=${CAMERA_DEVICE} ! image/jpeg,width=640, height=480, framerate=30/1 ! rtpjpegpay ! udpsink host=${IP} port=${STREAM_PORT} &
+
+    fi
 
 elif ([ "$INITIALIZE" == "powerdown" ]); then
 
-    docker container rm -f ${VEHICLE_NAME}
+    killall -9 gst-launch-1.0
 
 fi
